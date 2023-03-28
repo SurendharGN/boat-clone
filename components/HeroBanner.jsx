bdf98572f08bf9abd0aafec2c7145fb387d856e5
Surendhar <studyspace.ie@gmail.com>
2023-03-28 22:46:26 +0530

    completed front page

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -9,25 +9,26 @@ import {urlFor} from "../pages/index"
 
 const HeroBanner = ({heroBanner}) => {
   return (
-    <div className="bg-slate-100 h-[25rem] relative">
-        <div className="relative mb-40 m-10">
-            <h1 className='text-2xl'>{heroBanner.smallText}</h1>
-            <h2 className='text-5xl text-white'>Summer sale</h2>
-            <h3 className='text-6xl'>FINE</h3>
-            <img src={urlFor(heroBanner.image)} alt="headphones" width="300px" height="300px" className=" absolute right-20 top-0"/>
+    <div className=" rounded-xl bg-slate-100 flex w-11/12 justify-between m-auto h-[30rem] bg-gradient-to-r from-sky-300 to-sky-400">
+        <div className=" flex flex-col gap-5 mb-40 m-10">
+            <h1 className='text-7xl font-bold'>Introducing the all new {heroBanner.smallText}</h1>
+            <h2 className=' text-white font-bold text-7xl'>Summer sale</h2>
+            <Link href= {`/product/${heroBanner.product}`} className="bg-black w-32 h-32 justify-center  align-middle flex items-center mt-5 rounded-lg " >
+                <button type="button" className="my-3 text-white text-lg font-medium">{heroBanner.buttonText} </button>
+            </Link>
+        </div>
+
+        <div>
+            <img src={urlFor(heroBanner.image)} alt="headphones"  className="h-128"/>
         </div>
     
-        <div className="bg-sky-300 w-32 h-16 justify-center  align-middle flex items-center m-10  ">
-            <Link href= {`/product/${heroBanner.product}`} className="" >
-                <button type="button" className="">{heroBanner.buttonText} </button>
-            </Link>
+        
+            
         
         
-            <div className="absolute flex flex-col text-right right-10">
-                <h2 className=" ">Description</h2>
-                <p className=" ">{heroBanner.desc}</p>
-            </div>
-        </div>
+        
+          
+        
         
     </div>
   )
